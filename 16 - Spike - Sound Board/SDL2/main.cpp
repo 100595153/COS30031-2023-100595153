92c6a015daba36972b712b49aaa36643190356f8
Mitchell Wright <100595153@student.swin.edu.au>
2023-10-18 00:20:46 +1100

    Task 16 + 17 complete

--- 16 - Spike - Sound Board/SDL2/main.cpp
+++ 16 - Spike - Sound Board/SDL2/main.cpp
@@ -1,10 +1,24 @@
 #include <SDL.h>
+#include <SDL_mixer.h>
+#include <string>
 #include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+void playSound(string file)
+{
+	Mix_Chunk* sound = Mix_LoadWAV(file.c_str());
+	if (sound != nullptr)
+	{
+		Mix_PlayChannel(-1, sound, 0);
+	}
+}
 
 int main(int argc, char* argv[])
 {
 	//Init, init check, error message.
-	if (SDL_Init(SDL_INIT_VIDEO) != 0)
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0)
 	{
 		SDL_Log("SDL init failed: %s", SDL_GetError());
 		return 1;
@@ -16,7 +30,7 @@ int main(int argc, char* argv[])
 	{
 		SDL_Log("Window init failed: %s", SDL_GetError());
 		SDL_Quit();
-		return 2;
+		return 3;
 	}
 
 	//Renderer creation, check and error message.
@@ -26,14 +40,18 @@ int main(int argc, char* argv[])
 		SDL_Log("Renderer init failed: %s", SDL_GetError());
 		SDL_DestroyWindow(window);
 		SDL_Quit();
-		return 3;
+		return 4;
 	}
+
+	Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
+
+	Mix_Music* bgm = Mix_LoadMUS("oblivion.wav");
 
 	bool running = true;
 	SDL_Event event;
 
-	//Set screen to green.
-	SDL_SetRenderDrawColor(renderer, 0, 128, 0, 255);
+	//Set screen to black.
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 
 	//While open, check if I've quit, then render.
 	while (running)
@@ -43,15 +61,38 @@ int main(int argc, char* argv[])
 			if (event.type == SDL_QUIT)
 				running = false;
 
-			if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_r)
+			if (event.type == SDL_KEYDOWN)
 			{
-				SDL_SetRenderDrawColor(renderer, rand() % 256, rand() % 256, rand() % 256, 255);
+				switch (event.key.keysym.sym)
+				{
+				case SDLK_1:
+					playSound("potion_seller.wav");
+					break;
+				case SDLK_2:
+					playSound("im_going_into_battle.wav");
+					break;
+				case SDLK_3:
+					playSound("i_want_your_potions.wav");
+					SDL_Log("You can't handle my potions. My potions are too strong for you, traveller.");
+					break;
+				case SDLK_0:
+					if (Mix_PlayingMusic() == 0)
+						Mix_PlayMusic(bgm, -1);
+					else if (Mix_PausedMusic() == 1)
+						Mix_ResumeMusic();
+					else
+						Mix_PauseMusic();
+				default:
+					break;
+				}
 			}
 		}
 
 		SDL_RenderClear(renderer);
 		SDL_RenderPresent(renderer);
 	}
+
+
 
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
---
+++ 17 - Spike - Sprites and Graphics/SDL2/main.cpp
@@ -0,0 +1,115 @@
+#include <SDL.h>
+#include <SDL_mixer.h>
+#include <string>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+SDL_Rect randomisePos()
+{
+	int x, y;
+	x = rand() % 701;
+	y = rand() % 501;
+
+	SDL_Rect result = { x, y, 100, 100 };
+
+	return result;
+}
+
+int main(int argc, char* argv[])
+{
+	//Init, init check, error message.
+	if (SDL_Init(SDL_INIT_VIDEO) != 0)
+	{
+		SDL_Log("SDL init failed: %s", SDL_GetError());
+		return 1;
+	}
+
+	//Window creation, window check, error message.
+	SDL_Window* window = SDL_CreateWindow("Task 15", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+	if (!window)
+	{
+		SDL_Log("Window init failed: %s", SDL_GetError());
+		SDL_Quit();
+		return 3;
+	}
+
+	//Renderer creation, check and error message.
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	if (!window)
+	{
+		SDL_Log("Renderer init failed: %s", SDL_GetError());
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 4;
+	}
+
+	//Surface and Texture for background image.
+	SDL_Surface* imageSurface = SDL_LoadBMP("wamce.bmp");
+	SDL_Texture* bgTexture = SDL_CreateTextureFromSurface(renderer, imageSurface);
+	bool bgVisible = true;
+
+	//Surface and Texture for tile image.
+	SDL_Surface* tileSurface = SDL_LoadBMP("tile.bmp");
+	SDL_Texture* tileTexture = SDL_CreateTextureFromSurface(renderer, tileSurface);
+	
+	//Display rects
+	SDL_Rect tileRect = { 0, 0, 100, 100 };
+	SDL_Rect screenRect = randomisePos();
+
+	bool running = true;
+	SDL_Event event;
+
+	//While open, check if I've quit, then render.
+	while (running)
+	{
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_QUIT)
+				running = false;
+
+			if (event.type == SDL_KEYDOWN)
+			{
+				switch (event.key.keysym.sym)
+				{
+				case SDLK_1:
+					tileRect = { 0, 0, 100, 100 };
+					screenRect = randomisePos();
+					break;
+				case SDLK_2:
+					tileRect = { 100, 0, 100, 100 };
+					screenRect = randomisePos();
+					break;
+				case SDLK_3:
+					tileRect = { 200, 0, 100, 100 };
+					screenRect = randomisePos();
+					break;
+				case SDLK_0:
+					bgVisible = !bgVisible;
+					break;
+				default:
+					break;
+				}
+			}
+		}
+
+		SDL_RenderClear(renderer);
+
+		if (bgVisible)
+		{
+			SDL_RenderCopy(renderer, bgTexture, nullptr, nullptr);
+		}
+
+		SDL_RenderCopy(renderer, tileTexture, &tileRect, &screenRect);
+		SDL_RenderPresent(renderer);
+	}
+
+
+	SDL_FreeSurface(imageSurface);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return 0;
+}

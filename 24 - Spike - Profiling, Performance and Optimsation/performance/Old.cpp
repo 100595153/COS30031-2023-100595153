262491eeb1d9212291b0c61504874b13ab0f7828
Mitchell Wright <100595153@student.swin.edu.au>
2023-10-24 22:56:10 +1100

    Finished T24

--- 24 - Spike - Profiling, Performance and Optimsation/performance/Old.cpp
+++ 24 - Spike - Profiling, Performance and Optimsation/performance/Old.cpp
@@ -23,11 +23,10 @@ Clinton Woodward <cwoodward@swin.edu.au>
 Updated 2017-10-26
 
 */
-
+/*
 #include <iostream>
 #include <ctime>
 #include "SDL.h"
-#include <unordered_map>
 
 // using namespace std;
 
@@ -40,7 +39,7 @@ const int BOX_HEIGHT = 50;
 const int BOX_SPEED = 10;
 const int BOX_COUNT = 100;
 
-const int TEST_TIME = 10 * 1000; // ie, 3*1000 = 3 seconds
+const int TEST_TIME = 3 * 1000; // ie, 3*1000 = 3 seconds
 
 enum BoxState { CONTACT_NO, CONTACT_YES };
 
@@ -53,8 +52,6 @@ struct CrashBox {
 
 // Global variables. (Apparently evil.)
 CrashBox boxes[BOX_COUNT];
-SDL_Color white = { 255,255,255,255 };
-SDL_Color red = { 255,0,0,255 };
 
 void (*crash_test_all_ptr)(); // global function pointer!
 
@@ -68,29 +65,29 @@ void init_boxes()
 	// set each box to a random position, size, and velocity
 	for (int i = 0; i < BOX_COUNT; i++) {
 		// position
-		boxes[i].x = rand()%SCREEN_WIDTH;
-		boxes[i].y = rand()%SCREEN_HEIGHT;
+		boxes[i].x = rand() % SCREEN_WIDTH;
+		boxes[i].y = rand() % SCREEN_HEIGHT;
 		// size
-		boxes[i].w = (rand()%BOX_WIDTH)+1;
-		boxes[i].h = (rand()%BOX_HEIGHT)+1;
+		boxes[i].w = (rand() % BOX_WIDTH) + 1;
+		boxes[i].h = (rand() % BOX_HEIGHT) + 1;
 		// velocity (both positive and negative "delta" values)
-		boxes[i].dx = (rand()%(BOX_SPEED*2))-(BOX_SPEED/2);
-		boxes[i].dy = (rand()%(BOX_SPEED*2))-(BOX_SPEED/2);
+		boxes[i].dx = (rand() % (BOX_SPEED * 2)) - (BOX_SPEED / 2);
+		boxes[i].dy = (rand() % (BOX_SPEED * 2)) - (BOX_SPEED / 2);
 	}
 }
 
 
-void render_box(CrashBox &box, SDL_Renderer *renderer, SDL_Color &color) //TODO: try &box
+void render_box(CrashBox box, SDL_Renderer* renderer, SDL_Color& color) //TODO: try &box
 {
-	SDL_Rect r = { box.x, box.y, box.w, box.h}; //TODO: try cached rect's
+	SDL_Rect r = { box.x, box.y, box.w, box.h }; //TODO: try cached rect's
 	SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
 	SDL_RenderFillRect(renderer, &r);
 }
 
-void render_boxes(SDL_Renderer *renderer)
+void render_boxes(SDL_Renderer* renderer)
 {
-	//SDL_Color white = {255,255,255,255};
-	//SDL_Color red = {255,0,0,255};
+	SDL_Color white = { 255,255,255,255 };
+	SDL_Color red = { 255,0,0,255 };
 
 	for (int i = 0; i < BOX_COUNT; i++) {
 		if (boxes[i].state == CONTACT_NO) {
@@ -169,7 +166,7 @@ bool crash_test_B(CrashBox A, CrashBox B) // struct (copy)
 }
 
 
-bool crash_test_C(CrashBox &A, CrashBox &B) // via struct (ref!)
+bool crash_test_C(CrashBox& A, CrashBox& B) // via struct (ref!)
 {
 	//The sides of the rectangles
 	int leftA, leftB;
@@ -199,12 +196,12 @@ bool crash_test_C(CrashBox &A, CrashBox &B) // via struct (ref!)
 	return true;
 }
 
-bool crash_test_D(CrashBox &A, CrashBox &B)
+bool crash_test_D(CrashBox& A, CrashBox& B)
 {
 	if ((A.y + A.h) <= B.y) return false;
-	else if (A.y >= (B.y + B.h)) return false;
-	else if ((A.x + A.w) <= B.x) return false;
-	else if (A.x >= (B.x + B.w)) return false;
+	if (A.y >= (B.y + B.h)) return false;
+	if ((A.x + A.w) <= B.x) return false;
+	if (A.x >= (B.x + B.w)) return false;
 	return true;
 }
 
@@ -215,7 +212,7 @@ void crash_test_all_A1()
 	// check i against j
 	for (int i = 0; i < BOX_COUNT; i++) {
 		for (int j = 0; j < BOX_COUNT; j++) {
-			if (crash_test_A(i,j)) {
+			if (crash_test_A(i, j)) {
 				if (i != j) {
 					boxes[i].state = CONTACT_YES;
 					boxes[j].state = CONTACT_YES;
@@ -229,8 +226,8 @@ void crash_test_all_A2()
 {
 	// check i against j
 	for (int i = 0; i < BOX_COUNT; i++) {
-		for (int j = i+1; j < BOX_COUNT; j++) {
-			if (crash_test_A(i,j)) {
+		for (int j = i + 1; j < BOX_COUNT; j++) {
+			if (crash_test_A(i, j)) {
 				boxes[i].state = CONTACT_YES;
 				boxes[j].state = CONTACT_YES;
 			}
@@ -242,8 +239,8 @@ void crash_test_all_B()
 {
 	// check i against j
 	for (int i = 0; i < BOX_COUNT; i++) {
-		for (int j = i+1; j < BOX_COUNT; j++) {
-			if (crash_test_B(boxes[i],boxes[j])) {
+		for (int j = i + 1; j < BOX_COUNT; j++) {
+			if (crash_test_B(boxes[i], boxes[j])) {
 				boxes[i].state = CONTACT_YES;
 				boxes[j].state = CONTACT_YES;
 			}
@@ -255,8 +252,8 @@ void crash_test_all_C()
 {
 	// check i against j
 	for (int i = 0; i < BOX_COUNT; i++) {
-		for (int j = i+1; j < BOX_COUNT; j++) {
-			if (crash_test_C(boxes[i],boxes[j])) {
+		for (int j = i + 1; j < BOX_COUNT; j++) {
+			if (crash_test_C(boxes[i], boxes[j])) {
 				boxes[i].state = CONTACT_YES;
 				boxes[j].state = CONTACT_YES;
 			}
@@ -269,8 +266,21 @@ void crash_test_all_D()
 {
 	// check i against j
 	for (int i = 0; i < BOX_COUNT; i++) {
-		for (int j = i+1; j < BOX_COUNT; j++) {
-			if (crash_test_D(boxes[i],boxes[j])) {
+		for (int j = i + 1; j < BOX_COUNT; j++) {
+			if (crash_test_D(boxes[i], boxes[j])) {
+				boxes[i].state = CONTACT_YES;
+				boxes[j].state = CONTACT_YES;
+			}
+		}
+	}
+}
+
+void crash_test_all_()
+{
+	// check i against j
+	for (int i = 0; i < BOX_COUNT; i++) {
+		for (int j = i + 1; j < BOX_COUNT; j++) {
+			if (crash_test_D(boxes[i], boxes[j])) {
 				boxes[i].state = CONTACT_YES;
 				boxes[j].state = CONTACT_YES;
 			}
@@ -289,16 +299,15 @@ void update_boxes()
 		boxes[i].y = boxes[i].y + boxes[i].dy;
 		// check for wrap-around condition
 		if (boxes[i].x >= SCREEN_WIDTH) boxes[i].x -= SCREEN_WIDTH;
-		else if (boxes[i].x < 0) boxes[i].x += SCREEN_WIDTH;
-		else if (boxes[i].y >= SCREEN_HEIGHT) boxes[i].y -= SCREEN_HEIGHT;
-		else if (boxes[i].y < 0) boxes[i].y += SCREEN_HEIGHT;
+		if (boxes[i].x < 0) boxes[i].x += SCREEN_WIDTH;
+		if (boxes[i].y >= SCREEN_HEIGHT) boxes[i].y -= SCREEN_HEIGHT;
+		if (boxes[i].y < 0) boxes[i].y += SCREEN_HEIGHT;
 		//TODO: try else if logic
-		boxes[i].state = CONTACT_NO;
 	}
 
 	// 1. mark all boxes as not collided //TODO: put this in the move loop?
-	//for (int i = 0; i < BOX_COUNT; i++)
-		//boxes[i].state = CONTACT_NO;
+	for (int i = 0; i < BOX_COUNT; i++)
+		boxes[i].state = CONTACT_NO;
 	// 2. call whatever function has been set to test all i against j boxes
 	crash_test_all_ptr();
 }
@@ -306,15 +315,15 @@ void update_boxes()
 
 //-----------------------------------------------------------------------------
 
-int run_test(const char * title, void (*function_ptr)()) {
+int run_test(const char* title, void (*function_ptr)()) {
 	// get SDL to setup all subsystems
-  if ( SDL_Init( SDL_INIT_EVERYTHING ) == -1 ) {
+	if (SDL_Init(SDL_INIT_EVERYTHING) == -1) {
 		std::cout << " Failed to initialize SDL : " << SDL_GetError() << std::endl;
 		return -1;
-  }
+	}
 
-  // window with title and size we want
-	SDL_Window *window = SDL_CreateWindow(
+	// window with title and size we want
+	SDL_Window* window = SDL_CreateWindow(
 		title,
 		SDL_WINDOWPOS_UNDEFINED,
 		SDL_WINDOWPOS_UNDEFINED,
@@ -323,19 +332,19 @@ int run_test(const char * title, void (*function_ptr)()) {
 		SDL_WINDOW_SHOWN
 	);
 
-  if ( window == nullptr ) {
+	if (window == nullptr) {
 		std::cout << "Failed to create window : " << SDL_GetError();
 		return -1;
-  }
+	}
 
-  // renderer for the window ...
-	SDL_Renderer *renderer = SDL_CreateRenderer(
+	// renderer for the window ...
+	SDL_Renderer* renderer = SDL_CreateRenderer(
 		window,
 		-1,
 		0 //SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
 	);
 
-	if ( renderer == nullptr ) {
+	if (renderer == nullptr) {
 		std::cout << "Failed to create renderer : " << SDL_GetError();
 		return -1;
 	}
@@ -361,11 +370,11 @@ int run_test(const char * title, void (*function_ptr)()) {
 	// CLASSIC GAME LOOP
 	bool running = true;
 	SDL_Event event;
-	while (running)	{
+	while (running) {
 		// UPDATE
 		// 1. check for quit event
-		while( SDL_PollEvent( &event ) ) {
-				if( event.type == SDL_QUIT ) running = false;
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) running = false;
 		}
 		// 2. count...
 		loop_count++;
@@ -398,27 +407,22 @@ int run_test(const char * title, void (*function_ptr)()) {
 	// SHOW STATS
 	printf("Loops: %d\n", loop_count);
 	printf("Time: %d (ms)\n", (tick_end - tick_start));
-	printf("Loops/Second: %f\n", (float(loop_count) / (tick_end - tick_start)*1000.0));
+	printf("Loops/Second: %f\n", (float(loop_count) / (tick_end - tick_start) * 1000.0));
 
 	return 0;
 }
 
-int main( int argc, char* args[])
+int main(int argc, char* args[])
 {
-	if(run_test("Test A1", crash_test_all_A1) != 0) //TODO1
-		return -1;
-	if(run_test("Test A2", crash_test_all_A2) != 0)
-		return -1;
-	if (run_test("Test B", crash_test_all_B) != 0)
-		return -1;
-	if (run_test("Test C", crash_test_all_C) != 0)
-		return -1;
-	if (run_test("Test D", crash_test_all_D) != 0)
-		return -1;
+	run_test("Test A1", crash_test_all_A1); //TODO: not paying attention to return values. :(
+	run_test("Test A2", crash_test_all_A2);
+	run_test("Test B", crash_test_all_B);
+	run_test("Test C", crash_test_all_C);
+	run_test("Test D", crash_test_all_D);
 
 	printf("\n<press any key>\n");
 	std::getchar();
 
 	return 0;
-}
+}*/
 
